11375293e232f91d2dd94d8b87f8d8c38a1270ff
Oliver Zott <zott_oliver@web.de>
2022-11-07 18:06:34 +0100

    Ultimate Hello World - Dependency Injection

---
+++ UltimateHelloWorld/App.cs
@@ -0,0 +1,32 @@
+﻿using HelloWorldLibrary.BusinessLogic;
+
+namespace UltimateHelloWorld;
+
+public class App
+{
+    private readonly IMessages _messages;
+
+    public App(IMessages messages)
+    {
+        _messages = messages;
+    }
+
+
+    // Using: UltimateHelloWorld.exe -lang=it arg2 arg3
+    public void Run(string[] args)
+    {
+        var lang = "en";
+
+        foreach (var arg in args)
+        {
+            if (arg.ToLower().StartsWith("-lang"))
+            {
+                lang = arg.Substring(6);
+                break;
+            }
+        }
+
+        var message = _messages.Greeting(lang);
+        Console.WriteLine(message);
+    }
+}
--- UltimateHelloWorld/Program.cs
+++ UltimateHelloWorld/Program.cs
@@ -1,22 +1,57 @@
 ﻿using HelloWorldLibrary.BusinessLogic;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UltimateHelloWorld;
 
-public static class MainProgram
+// for proper disposing of host variable (releasing resources)
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+
+try
 {
-    public static void Main()
-    {
-        using var loggerFactory = LoggerFactory.Create(builder =>
-        {
-            builder
-                .AddFilter("Microsoft", LogLevel.Warning)
-                .AddFilter("System", LogLevel.Warning)
-                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug);
-        });
-        ILogger<Messages> logger = loggerFactory.CreateLogger<Messages>();
-
-        var messages = new Messages(logger);
-        var message = messages.Greeting("it");
-
-        Console.WriteLine(message);
-    }
+    services.GetRequiredService<App>().Run(args);
 }
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            services.AddSingleton<IMessages, Messages>();
+            services.AddSingleton<App>();
+        });
+}
+
+
+
+
+
+
+
+//public static class MainProgram
+//{
+//    public static void Main()
+//    {
+//        using var loggerFactory = LoggerFactory.Create(builder =>
+//        {
+//            builder
+//                .AddFilter("Microsoft", LogLevel.Warning)
+//                .AddFilter("System", LogLevel.Warning)
+//                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug);
+//        });
+//        ILogger<Messages> logger = loggerFactory.CreateLogger<Messages>();
+
+//        var messages = new Messages(logger);
+//        var message = messages.Greeting("it");
+
+//        Console.WriteLine(message);
+//    }
+//}
